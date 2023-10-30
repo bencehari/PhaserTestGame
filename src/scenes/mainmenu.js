9d5e93aa5ba5d09bfa28746c6f7ac771b284e079
h4ri <bencehari@gmail.com>
2023-10-30 23:31:04 +0100

    player level xp req experiment

--- src/scenes/mainmenu.js
+++ src/scenes/mainmenu.js
@@ -6,9 +6,27 @@ class MainMenu extends Phaser.Scene {
 
         const text = this.add.text(this.game.config.width * 0.5, this.game.config.height * 0.5, 'press space to start game', getDefaultTextStyle(30))
         text.setOrigin(0.5, 0.5)
+
+        // this.printPlayerLevelXPReqs()
     }
 
     update() {
         if (this.#spaceBar.isDown) this.scene.start('play', { lives: 3, level: 1 })
+    }
+
+    printPlayerLevelXPReqs() {
+        let xp0 = 0
+        let xp1 = 5
+        let xp = 5
+        let mod = 1.5
+        let modStep = 0.001
+        for (let i = 2; i < 100; i++) {
+            console.log(`level: ${i}, xp: ${xp}`)
+
+            xp += Math.ceil(Math.ceil(xp1 - xp0) * mod)
+            xp0 = xp1
+            xp1 = xp
+            mod += modStep
+        }
     }
 }
